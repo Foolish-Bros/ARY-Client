e0270d5d0aed1873ee8f865d56a0ec0b874ddc09
Kim Seongjin <darkmoon625@naver.com>
2024-05-26 22:36:17 +0900

    Fix some button UI

--- src/component/ChatMessageInputBox.js
+++ src/component/ChatMessageInputBox.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
 
 function ChatMessageInputBox({ inputValue, setInputValue, addMessage }) {
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px', mt: 2 }}>
-      <TextField
-        fullWidth
-        variant="outlined"
-        placeholder="메시지를 입력하세요"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') {
-            addMessage();
-          }
-        }}
-      />
-      <Button
-        variant="contained"
-        onClick={addMessage}
-        sx={{
-          marginLeft: '8px',
-          backgroundColor: '#007f73',
-          fontWeight: 'bold',
-          '&:hover': {
-            backgroundColor: '#00695c',
-          },
-        }}
-      >
-        전송
-      </Button>
-    </Box>
-  );
+	return (
+		<Box sx={{ display: "flex", alignItems: "center", gap: "8px", mt: 2 }}>
+			<TextField
+				fullWidth
+				variant="outlined"
+				placeholder="메시지를 입력하세요"
+				value={inputValue}
+				onChange={(e) => setInputValue(e.target.value)}
+				onKeyPress={(e) => {
+					if (e.key === "Enter") {
+						addMessage();
+					}
+				}}
+			/>
+			<Button
+				variant="contained"
+				onClick={addMessage}
+				sx={{
+					marginLeft: "8px",
+					backgroundColor: "#007f73",
+					fontWeight: "bold",
+					height: "56px",
+					"&:hover": {
+						backgroundColor: "#00695c",
+					},
+				}}
+			>
+				전송
+			</Button>
+		</Box>
+	);
 }
 
 export default ChatMessageInputBox;
